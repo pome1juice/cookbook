975cbef32e44147430a0227c6eb6d6b01e316a5a
Stephanie Goffin <skgoffin@ntlworld.com>
2018-08-25 13:38:45 +0000

    req doc1

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'cook_book'
+app.config["MONGO_URL"] = 'mongodb://<dbuser>:<dbpassword>@ds125402.mlab.com:25402/cook_book'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello'
+@app.route('/get_recepies')
+def get_recepies():
+    return render_template("recepies.html", 
+    recepies=mongo.db.recepies.find())
+
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
