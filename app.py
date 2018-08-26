c07a902153b452f6c18013e0210a9d75f1da1d0a
Stephanie Goffin <skgoffin@ntlworld.com>
2018-08-26 20:42:42 +0000

    end sunday, only vote left

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def checkredundant(thisname):
         return True
     else:
         return False
-        
+
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'cook_book'
 app.config["MONGO_URI"] = 'mongodb://admin:c9user@ds125402.mlab.com:25402/cook_book'
@@ -22,6 +22,26 @@ mongo = PyMongo(app)
 def get_recepies():
     return render_template("recepies.html", 
     recepies=mongo.db.recepies.find())
+    
+@app.route('/get_recepies_ofcat/<category_name>')
+def get_recepies_ofcat(category_name):
+    myquery = { "category_name": category_name }
+    recepies=mongo.db.recepies.find(myquery)
+    this_list=[rec for rec in recepies]
+    if not this_list:
+        return '<h1 style="text-align:center; margin-top:5em;">Sorry there was no such recepies with this category, please go back</h1>'
+    return render_template("recepiescat.html", 
+    recepies=mongo.db.recepies.find(myquery), category_name=category_name)
+    
+@app.route('/get_recepies_ofdie/<diet_name>')
+def get_recepies_ofdie(diet_name):
+    myquery = { "diet_name":diet_name }
+    recepies=mongo.db.recepies.find(myquery)
+    this_list=[rec for rec in recepies]
+    if not this_list:
+        return '<h1 style="text-align:center; margin-top:5em;">Sorry there was no such recepies with this diet, please go back</h1>'
+    return render_template("recepiesdiet.html", 
+    recepies=mongo.db.recepies.find(myquery), diet_name=diet_name)
 
 @app.route('/add_recepie')
 def add_recepie():
@@ -41,7 +61,7 @@ def insert_recepie():
         recepies.insert_one(rec_dict)
         return redirect(url_for('get_recepies'))
     else:
-        return 'Sorry there was a duplicate recepie in our system please go back'
+        return '<h1 style="text-align:center; margin-top:5em;">Sorry there was a duplicate recepie in our system please go back</h1>'
     
 @app.route('/edit_recepie/<recepie_id>') 
 def edit_recepie(recepie_id):
@@ -70,22 +90,20 @@ def like_recepie(recepie_id):
 
 @app.route('/update_recepie/<recepie_id>', methods=["POST"])
 def update_recepie(recepie_id):
+    rec_dict = request.form.to_dict()
     rec = mongo.db.recepies.find_one({"_id": ObjectId(recepie_id)})
-    if checkredundant(request.form["recepie_name"]):
-        recepies = mongo.db.recepies
-        recepies.update( {'_id': ObjectId(recepie_id)},
-        {
-            'recepie_votes': int(rec['recepie_votes']),
-            'recepie_name':request.form.get['recepie_name'],
-            'category_name':request.form.get['category_name'],
-            'recepie_ins':request.form.get['recepie_ins'],
-            'recepie_ingredients':request.form.get['recepie_ingredients'],
-            'diet_name':request.form.get['diet_name'],
-            'recepie_img':request.form.get['recepie_img']
-        })
-        return redirect(url_for('get_recepies'))
-    else:
-        return 'Sorry there was a duplicate recepie in our system please go back'
+    recepies = mongo.db.recepies
+    recepies.update( {'_id': ObjectId(recepie_id)},
+    {
+        'recepie_votes': int(rec['recepie_votes']),
+        'recepie_name':rec_dict['recepie_name'],
+        'category_name':rec_dict['category_name'],
+        'recepie_ins':rec_dict['recepie_ins'],
+        'recepie_ingredients':rec_dict['recepie_ingredients'],
+        'diet_name':rec_dict['diet_name'],
+        'recepie_img':rec_dict['recepie_img']
+    })
+    return redirect(url_for('get_recepies'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
